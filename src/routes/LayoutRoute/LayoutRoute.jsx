609eb856498ba5ba1e73a0c3d163f5724f8574ac
Sadik <sadik.al.sami.2002@gmail.com>
2024-01-30 15:21:23 +0600

    error page created and working

--- src/routes/LayoutRoute/LayoutRoute.jsx
+++ src/routes/LayoutRoute/LayoutRoute.jsx
@@ -3,11 +3,13 @@ import MainLayout from '../../layouts/MainLayout';
 import Login from '../../auth/Login/Login';
 import HomeLayout from '../../layouts/HomeLayout';
 import Chefs from '../../components/chefs/chefs';
+import Error from '../../components/Shared/Error/Error';
 
 const LayoutRoute = createBrowserRouter([
   {
     path: '/',
     element: <MainLayout />,
+    errorElement: <Error/>,
     children: [
       {
         path: '/',
