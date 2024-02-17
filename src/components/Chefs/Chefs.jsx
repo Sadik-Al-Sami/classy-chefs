8f4307ff93d5c9dd188ff14efd133d2e9c222f69
Sadik <sadik.al.sami.2002@gmail.com>
2024-02-17 15:57:57 +0600

    added chef data on recipe page

--- src/components/Chefs/Chefs.jsx
+++ src/components/Chefs/Chefs.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Chef from '../Chef/Chef';
+import { AuthContext } from '../../providers/AuthProvider';
 
 const Chefs = () => {
   const chefsData = useLoaderData();
+  const { loading } = useContext(AuthContext);
   return (
     <div className='bg-base-200'>
+      {loading && (
+        <div className='container mx-auto'>
+          <div className='h-screen flex justify-center items-center'>
+            <PacmanLoader
+              size={100}
+              color='#36d7b7'
+            />
+          </div>
+        </div>
+      )}
       <div className='grid md:grid-cols-2 lg:grid-cols-4 container mx-auto'>
-        {setTimeout(() => {
-          chefsData.map((chef) => (
-            <Chef
-              key={chef.id}
-              chef={chef}></Chef>
-          ));
-        }, 5000)}
+        {chefsData.map((chef) => (
+          <Chef
+            key={chef.id}
+            chef={chef}></Chef>
+        ))}
       </div>
     </div>
   );
--- src/components/RecipePage/RecipePage.jsx
+++ src/components/RecipePage/RecipePage.jsx
@@ -11,20 +11,38 @@ const RecipePage = () => {
       .then((data) => setChefsData(data))
       .catch((error) => console.log(error));
   }, [chef_id]);
-  console.log(recipeData);
+  // console.log(recipeData);
   console.log(chefData);
+  const c_name = chefData.name;
+  const { picture, recipes, years_of_experience, likes } = chefData;
   return (
     <div className='bg-base-200'>
       <div className='container bg-base-300 rounded-xl shadow-md mx-auto p-10 font-poppins'>
         <h1 className='mt-20 text-center text-4xl'>{name}</h1>
+        <div className='grid md:grid-cols-2 mt-10'>
+          <div>
+            <img
+              className='rounded-md'
+              src='https://source.unsplash.com/400x400/?profile'
+              alt={picture}
+            />
+          </div>
+          <div>
+            {/* Chef Details */}
+            <div className='flex flex-col text-lg'>
+              <p>
+                <span className='font-semibold'>Chef Name</span> : {c_name}
+              </p>
+              <p>
+                <span className='font-semibold'>Likes on this recipe</span> :{' '}
+                {likes}
+              </p>
+            </div>
+          </div>
+        </div>
         <div className='grid md:grid-cols-2'>
           {/* Details */}
           <div className='row-span-4 mt-20'>
-            {/* Chef Details */}
-            <div className='flex flex-col text-lg'>
-              <p>Chef ID: {chef_id}</p>
-              <p>Recipe Number: {id}</p>
-            </div>
             {/* Recipe Details */}
             <div className='mt-4'>
               <h3 className='text-xl font-bold tracking-wide'>Ingredients</h3>
